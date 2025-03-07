b5752fb33243514001df1f4126221896ca20c995
Elif Yalcin <elif.yalcin@intertech.com.tr>
2025-03-07 17:43:21 +0300

    Initial Commit

---
+++ LocationTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationTracker
+//
+//  Created by ELIFYAL on 7.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
